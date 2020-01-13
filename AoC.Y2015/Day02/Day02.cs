820156c0dfc5a1adea685623f18f882be409f9b9
EwoutIO <ewout@schrotenboer.nl>
2020-01-13 08:25:41 +0100

    Day 2: I Was Told There Would Be No Math

--- AoC.Y2015/Day02/Day02.cs
+++ AoC.Y2015/Day02/Day02.cs
@@ -1,6 +1,7 @@
 ﻿using AoC.Helpers.Days;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC.Y2015.Days
 {
@@ -14,8 +15,52 @@ namespace AoC.Y2015.Days
         {
         }
 
-        protected override IConvertible PartOne() => throw new NotImplementedException();
+        protected override IConvertible PartOne()
+        {
+            var boxes = ParseInput(inputLines);
+            var sum = 0;
 
-        protected override IConvertible PartTwo() => throw new NotImplementedException();
+            foreach (var (l, w, h) in boxes)
+            {
+                // surface
+                sum += 2 * l * w;
+                sum += 2 * w * h;
+                sum += 2 * h * l;
+
+                // slack
+                var orderedBySize = new List<int>() { l, w, h }.OrderBy(p => p).ToList();
+                sum += orderedBySize[0] * orderedBySize[1];
+            }
+
+            return sum;
+        }
+
+        protected override IConvertible PartTwo()
+        {
+            var boxes = ParseInput(inputLines);
+            var sum = 0;
+
+            foreach (var (l, w, h) in boxes)
+            {
+                var orderedBySize = new List<int>() { l, w, h }.OrderBy(p => p).ToList();
+                sum += orderedBySize[0] * 2 + orderedBySize[1] * 2;
+                sum += l * w * h;
+            }
+
+            return sum;
+        }
+
+        private static List<(int l, int w, int h)> ParseInput(IEnumerable<string> inputLines)
+        {
+            var boxes = new List<(int l, int w, int h)>();
+
+            foreach (var line in inputLines)
+            {
+                var d = line.Split("x");
+                boxes.Add((int.Parse(d[0]), int.Parse(d[1]), int.Parse(d[2])));
+            }
+
+            return boxes;
+        }
     }
 }
