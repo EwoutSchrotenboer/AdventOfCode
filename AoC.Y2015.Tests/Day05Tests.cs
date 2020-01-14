297f5738d34a7f4fa1d0c0f7d9db5d6c6e82fb03
EwoutIO <ewout@schrotenboer.nl>
2020-01-14 13:31:37 +0100

    Day 5: Doesn't He Have Intern-Elves For This?

--- AoC.Y2015.Tests/Day05Tests.cs
+++ AoC.Y2015.Tests/Day05Tests.cs
@@ -8,7 +8,43 @@ namespace AoC.Y2015.Tests.Days
     public class Day05Tests
     {
         [TestMethod]
-        public void PartOneTest()
+        [DataRow("ugknbfddgicrmopn", 1)]
+        [DataRow("jchzalrnumimnmhp", 0)]
+        [DataRow("haegwjzuvuyypxyu", 0)]
+        [DataRow("dvszwmarrgswjxmb", 0)]
+        public void PartOneTest(string input, int expected)
+        {
+            // Arrange
+            var target = new Day05(new string[] { input });
+
+            // Act
+            var result = target.Debug(Part.One);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [DataRow("qjhvhtzxzqqjkmpb", 1)]
+        [DataRow("xxyxx", 1)]
+        [DataRow("uurcxstgmygtbstg", 0)]
+        [DataRow("ieodomkazucvgmuy", 0)]
+        public void PartTwoTest(string input, int expected)
+        {
+            // Arrange
+            var target = new Day05(new string[] { input });
+
+            // Act
+            var result = target.Debug(Part.Two);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void PartOne()
         {
             // Arrange
             var target = new Day05();
@@ -18,10 +54,11 @@ namespace AoC.Y2015.Tests.Days
 
             // Assert
             Assert.IsNotNull(result);
+            Assert.AreEqual(238, result);
         }
 
         [TestMethod]
-        public void PartTwoTest()
+        public void PartTwo()
         {
             // Arrange
             var target = new Day05();
@@ -31,6 +68,7 @@ namespace AoC.Y2015.Tests.Days
 
             // Assert
             Assert.IsNotNull(result);
+            Assert.AreEqual(69, result);
         }
     }
 }
--- AoC.Y2015/Day05/Day05.cs
+++ AoC.Y2015/Day05/Day05.cs
@@ -1,6 +1,7 @@
 ﻿using AoC.Helpers.Days;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC.Y2015.Days
 {
@@ -14,8 +15,57 @@ namespace AoC.Y2015.Days
         {
         }
 
-        protected override IConvertible PartOne() => throw new NotImplementedException();
+        protected override IConvertible PartOne() => inputLines.Count(i => FirstNiceCheck(i));
 
-        protected override IConvertible PartTwo() => throw new NotImplementedException();
+        protected override IConvertible PartTwo() => inputLines.Count(i => SecondNiceCheck(i));
+        private static bool FirstNiceCheck(string input)
+        {
+            if (ContainsDisallowed(input)) { return false; }
+            return ContainsThreeVowels(input) && ContainsTwiceInARow(input);
+        }
+
+        private static bool ContainsThreeVowels(string input) => input.Count(c => vowels.Contains(c)) >= 3;
+        private static bool ContainsDisallowed(string input) => disallowedStrings.Any(d => input.Contains(d));
+        private static bool ContainsTwiceInARow(string input)
+        {
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] == input[i+1]) { return true; }
+            }
+
+            return false;
+        }
+
+        private static bool SecondNiceCheck(string input) => ContainsDoublePair(input) && ContainsSeparatedPair(input);
+
+        private static bool ContainsDoublePair(string input)
+        {
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                for (int mi = i; mi < input.Length - 1; mi++)
+                {
+                    if (mi == i - 1 || mi == i || mi == i + 1) { continue; }
+                    if (input[i] == input[mi] && input[i + 1] == input[mi + 1])
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private static bool ContainsSeparatedPair(string input)
+        {
+            for (int i = 0; i < input.Length - 2; i++)
+            {
+                if (input[i] == input[i + 2]) { return true; }
+            }
+
+            return false;
+        }
+
+        private static char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+        private static string[] disallowedStrings = new string[] { "ab", "cd", "pq", "xy" };
     }
 }
