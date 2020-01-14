5d3c718228f29e010c34e2dabda3d2e5f18e810f
EwoutIO <ewout@schrotenboer.nl>
2020-01-14 17:02:47 +0100

    Day 7: Some Assembly Required

--- AoC.Y2015.Tests/Day07Tests.cs
+++ AoC.Y2015.Tests/Day07Tests.cs
@@ -8,7 +8,22 @@ namespace AoC.Y2015.Tests.Days
     public class Day07Tests
     {
         [TestMethod]
-        public void PartOneTest()
+        [DataRow(new string[] { "123 -> x", "456 -> y", "x AND y -> d", "x OR y -> e", "x LSHIFT 2 -> f", "y RSHIFT 2 -> g", "NOT x -> h", "NOT y -> i" }, 65079)]
+        public void PartOneTest(string[] input, int expected)
+        {
+            // Arrange
+            var target = new Day07(input);
+
+            // Act
+            var result = target.Debug(Part.One);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expected, (ushort)result);
+        }
+
+        [TestMethod]
+        public void PartOne()
         {
             // Arrange
             var target = new Day07();
@@ -18,10 +33,11 @@ namespace AoC.Y2015.Tests.Days
 
             // Assert
             Assert.IsNotNull(result);
+            Assert.AreEqual(16076, (ushort)result);
         }
 
         [TestMethod]
-        public void PartTwoTest()
+        public void PartTwo()
         {
             // Arrange
             var target = new Day07();
@@ -31,6 +47,7 @@ namespace AoC.Y2015.Tests.Days
 
             // Assert
             Assert.IsNotNull(result);
+            Assert.AreEqual(2797, (ushort)result);
         }
     }
 }
