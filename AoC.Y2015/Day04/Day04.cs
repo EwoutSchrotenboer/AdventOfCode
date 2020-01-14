04c5498aeb3a5981054d3462fc4ddb48adf62c81
EwoutIO <ewout@schrotenboer.nl>
2020-01-14 07:16:07 +0100

    Day 4: The Ideal Stocking Stuffer

--- AoC.Y2015/Day04/Day04.cs
+++ AoC.Y2015/Day04/Day04.cs
@@ -1,6 +1,8 @@
 ﻿using AoC.Helpers.Days;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace AoC.Y2015.Days
 {
@@ -14,8 +16,38 @@ namespace AoC.Y2015.Days
         {
         }
 
-        protected override IConvertible PartOne() => throw new NotImplementedException();
+        protected override IConvertible PartOne() => LeadingZeros(inputLines.Single(), 5);
 
-        protected override IConvertible PartTwo() => throw new NotImplementedException();
+        protected override IConvertible PartTwo() => LeadingZeros(inputLines.Single(), 6);
+
+
+        private static int LeadingZeros(string input, int zeroCount)
+        {
+            var num = 0;
+            var md5 = System.Security.Cryptography.MD5.Create();
+
+            while (true)
+            {
+                var inputBytes = Encoding.ASCII.GetBytes($"{input}{num}");
+                var hash = md5.ComputeHash(inputBytes);
+
+                var match = true;
+
+                for (int i = 0; i < zeroCount / 2; i++)
+                {
+                    match &= hash[i] == 0;
+                }
+
+                if (zeroCount % 2 != 0)
+                {
+                    // To check if the first four values are 0, shift them to the right, AND the number with 00001111, check if this equals 0
+                    match &= (hash[zeroCount / 2] >> 4 & 0xF) == 0;
+                }
+
+                if (match) { return num; }
+
+                num++;
+            }
+        }
     }
 }
