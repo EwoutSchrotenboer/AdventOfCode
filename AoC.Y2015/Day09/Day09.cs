00153aa7b3acaed1cbae549f2ef72e3b7b7bdebd
EwoutIO <ewout@schrotenboer.nl>
2020-01-17 06:55:48 +0100

    Day 9: All in a Single Night

--- AoC.Y2015/Day09/Day09.cs
+++ AoC.Y2015/Day09/Day09.cs
@@ -1,6 +1,8 @@
 ﻿using AoC.Helpers.Days;
+using AoC.Helpers.Utils;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC.Y2015.Days
 {
@@ -14,8 +16,45 @@ namespace AoC.Y2015.Days
         {
         }
 
-        protected override IConvertible PartOne() => throw new NotImplementedException();
+        protected override IConvertible PartOne() => GetRoutePermutations(ParseInput(inputLines)).Min();
 
-        protected override IConvertible PartTwo() => throw new NotImplementedException();
+        protected override IConvertible PartTwo() => GetRoutePermutations(ParseInput(inputLines)).Max();
+
+        private static List<int> GetRoutePermutations(Dictionary<(string src, string dst), int> edges)
+        {
+            var vertices = edges.Keys.Select(e => e.src).Distinct().OrderBy(v => v).ToList();
+            var permutations = vertices.GetPermutations(vertices.Count());
+            var distances = new List<int>();
+
+            // note, the brute force only works because of the small sample set
+            foreach (var p in permutations)
+            {
+                var route = p.ToList();
+                var distance = 0;
+
+                for (int i = 0; i < p.Count() - 1; i++)
+                {
+                    distance += edges[(route[i], route[i + 1])];
+                }
+
+                distances.Add(distance);
+            }
+
+            return distances;
+        }
+
+        private static Dictionary<(string src, string dst), int> ParseInput(IEnumerable<string> inputLines)
+        {
+            var edges = new Dictionary<(string, string), int>();
+
+            foreach (var line in inputLines)
+            {
+                var items = line.Split(' ');
+                edges.Add((items[0], items[2]), int.Parse(items[4]));
+                edges.Add((items[2], items[0]), int.Parse(items[4]));
+            }
+
+            return edges;
+        }
     }
 }
