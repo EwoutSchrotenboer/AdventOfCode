a9b39f64cbbf10fcc70ddaf5a9bf1a9e46488c80
EwoutIO <ewout@schrotenboer.nl>
2020-01-19 15:00:12 +0100

    Replaced brute force with decent algorithm

--- AoC.Y2015/Day09/Day09.cs
+++ AoC.Y2015/Day09/Day09.cs
@@ -16,45 +16,75 @@ namespace AoC.Y2015.Days
         {
         }
 
-        protected override IConvertible PartOne() => GetRoutePermutations(ParseInput(inputLines)).Min();
+        protected override IConvertible PartOne() => GetRoute(ParseInput(inputLines), true);
 
-        protected override IConvertible PartTwo() => GetRoutePermutations(ParseInput(inputLines)).Max();
+        protected override IConvertible PartTwo() => GetRoute(ParseInput(inputLines), false);
 
-        private static List<int> GetRoutePermutations(Dictionary<(string src, string dst), int> edges)
+        private static int GetRoute(Dictionary<int, Dictionary<int, int>> edges, bool getShortest)
         {
-            var vertices = edges.Keys.Select(e => e.src).Distinct().OrderBy(v => v).ToList();
-            var permutations = vertices.GetPermutations(vertices.Count());
-            var distances = new List<int>();
+			var target = 0;
+			for (var i = 0; i < edges.Count; ++i) { target |= (int)Math.Pow(2, i); }
 
-            // note, the brute force only works because of the small sample set
-            foreach (var p in permutations)
+			var visited = new Dictionary<(int, int), int>();
+			var routeQueue = new Queue<(int, int, int)>();
+
+			foreach (var location in edges.Keys) { routeQueue.Enqueue((location, location, 0)); }
+
+			var returnValue = getShortest ? int.MaxValue : int.MinValue;
+
+			while (routeQueue.Any())
+			{
+				var (currPosition, currVisited, currDistance) = routeQueue.Dequeue();
+				var value = (currPosition, currVisited);
+				if (visited.TryGetValue(value, out var distance))
+				{
+					if ((distance <= currDistance && getShortest) || (distance >= currDistance && !getShortest)) { continue; }
+					visited[value] = distance;
+				}
+				else
+				{
+					visited.Add(value, currDistance);
+				}
+
+				if (currVisited == target)
+				{
+					returnValue = getShortest ? Math.Min(returnValue, currDistance) : Math.Max(returnValue, currDistance);
+					continue;
+				}
+
+				foreach (var edge in edges[currPosition])
+				{
+					if ((currVisited & edge.Key) == edge.Key) { continue; }
+					routeQueue.Enqueue((edge.Key, currVisited | edge.Key, currDistance + edge.Value));
+				}
+			}
+
+			return returnValue;
+		}
+
+        private static Dictionary<int, Dictionary<int, int>> ParseInput(IEnumerable<string> inputLines)
+        {
+            var positions = new Dictionary<int, Dictionary<int, int>>();
+            var splitLines = inputLines.Select(l => l.Split(' '));
+            var positionNames = splitLines.SelectMany(l => new string[] { l[0], l[2] }).Distinct().ToArray();
+
+            var mapping = new Dictionary<string, int>();
+            for (int i = 0; i < positionNames.Length; i++) { mapping.Add(positionNames[i], (int)Math.Pow(2, i)); }
+
+            foreach (var line in splitLines)
             {
-                var route = p.ToList();
-                var distance = 0;
+                var src = mapping[line[0]];
+                var dst = mapping[line[2]];
+                var dist = int.Parse(line[4]);
 
-                for (int i = 0; i < p.Count() - 1; i++)
-                {
-                    distance += edges[(route[i], route[i + 1])];
-                }
+                if (!positions.ContainsKey(src)) { positions.Add(src, new Dictionary<int, int>()); }
+                positions[src].Add(dst, dist);
 
-                distances.Add(distance);
+                if (!positions.ContainsKey(dst)) { positions.Add(dst, new Dictionary<int, int>()); }
+                positions[dst].Add(src, dist);
             }
 
-            return distances;
-        }
-
-        private static Dictionary<(string src, string dst), int> ParseInput(IEnumerable<string> inputLines)
-        {
-            var edges = new Dictionary<(string, string), int>();
-
-            foreach (var line in inputLines)
-            {
-                var items = line.Split(' ');
-                edges.Add((items[0], items[2]), int.Parse(items[4]));
-                edges.Add((items[2], items[0]), int.Parse(items[4]));
-            }
-
-            return edges;
+            return positions;
         }
     }
 }
