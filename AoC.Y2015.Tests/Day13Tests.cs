923145baeb8bcb3cf4652183d9841156a8e5fb86
EwoutIO <ewout@schrotenboer.nl>
2020-01-24 07:43:58 +0100

    Day 13: Knights of the Dinner Table

--- AoC.Y2015.Tests/Day13Tests.cs
+++ AoC.Y2015.Tests/Day13Tests.cs
@@ -8,7 +8,27 @@ namespace AoC.Y2015.Tests.Days
     public class Day13Tests
     {
         [TestMethod]
-        public void PartOneTest()
+        [DataRow(new string[] { "Alice would gain 54 happiness units by sitting next to Bob.", "Alice would lose 79 happiness units by sitting next to Carol.", 
+                                "Alice would lose 2 happiness units by sitting next to David.", "Bob would gain 83 happiness units by sitting next to Alice.", 
+                                "Bob would lose 7 happiness units by sitting next to Carol.", "Bob would lose 63 happiness units by sitting next to David.", 
+                                "Carol would lose 62 happiness units by sitting next to Alice.", "Carol would gain 60 happiness units by sitting next to Bob.", 
+                                "Carol would gain 55 happiness units by sitting next to David.", "David would gain 46 happiness units by sitting next to Alice.", 
+                                "David would lose 7 happiness units by sitting next to Bob.", "David would gain 41 happiness units by sitting next to Carol.", }, 330)]
+        public void PartOneTest(string[] input, int expected)
+        {
+            // Arrange
+            var target = new Day13(input);
+
+            // Act
+            var result = target.Debug(Part.One);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void PartOne()
         {
             // Arrange
             var target = new Day13();
@@ -21,7 +41,7 @@ namespace AoC.Y2015.Tests.Days
         }
 
         [TestMethod]
-        public void PartTwoTest()
+        public void PartTwo()
         {
             // Arrange
             var target = new Day13();
--- AoC.Y2015/Day13/Day13.cs
+++ AoC.Y2015/Day13/Day13.cs
@@ -1,6 +1,8 @@
 ﻿using AoC.Helpers.Days;
+using AoC.Helpers.Utils;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC.Y2015.Days
 {
@@ -14,8 +16,59 @@ namespace AoC.Y2015.Days
         {
         }
 
-        protected override IConvertible PartOne() => throw new NotImplementedException();
+        protected override IConvertible PartOne() => DetermineBestScore(ParseInput(inputLines), false);
 
-        protected override IConvertible PartTwo() => throw new NotImplementedException();
-    }
+		protected override IConvertible PartTwo() => DetermineBestScore(ParseInput(inputLines), true);
+
+		private static int DetermineBestScore(Dictionary<(string first, string second), int> relations, bool partTwo)
+		{
+			var names = relations.Select(r => r.Key.first).Distinct().ToList();
+
+			if (partTwo)
+			{
+				foreach (var name in names)
+				{
+					relations.Add((name, "X"), 0);
+					relations.Add(("X", name), 0);
+				}
+
+				names.Add("X");
+			}
+
+			var permutations = names.GetPermutations(names.Count());
+			var bestScore = int.MinValue;
+
+			// Pretty much brute force, might optimize later if I feel like it.
+			foreach (var permutation in permutations)
+			{
+				var permutationScore = 0;
+				var seating = permutation.ToList();
+				var lastElementIndex = seating.Count() - 1;
+
+				for (int i = 0; i < seating.Count(); i++)
+				{
+					permutationScore += relations[(seating[i], i == 0 ? seating[lastElementIndex] : seating[i - 1])];
+					permutationScore += relations[(seating[i], i == lastElementIndex ? seating[0] : seating[i + 1])];
+				}
+
+				bestScore = Math.Max(bestScore, permutationScore);
+			}
+
+			return bestScore;
+		}
+
+		private static Dictionary<(string first, string second), int> ParseInput(IEnumerable<string> inputLines)
+		{
+			var relations = new Dictionary<(string, string), int>();
+
+			var splitLines = inputLines.Select(l => l.Split(' '));
+
+			foreach (var line in splitLines)
+			{
+				relations.Add((line[0], line[10].Replace(".", "")), int.Parse(line[3]) * (line[2] == "gain" ? 1 : -1));
+			}
+
+			return relations;
+		}
+	}
 }
