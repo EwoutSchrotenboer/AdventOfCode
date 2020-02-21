4d457d2cc2ad90b81e7e65189abf921292cd0712
EwoutIO <ewout@schrotenboer.nl>
2020-02-21 20:24:00 +0100

    Day 1: No Time for a Taxicab

--- AoC.Y2016/Day01/Day01.cs
+++ AoC.Y2016/Day01/Day01.cs
@@ -1,6 +1,8 @@
 ﻿using AoC.Helpers.Days;
+using AoC.Helpers.Utils;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC.Y2016.Days
 {
@@ -14,8 +16,52 @@ namespace AoC.Y2016.Days
         {
         }
 
-        protected override IConvertible PartOne() => throw new NotImplementedException();
+        protected override IConvertible PartOne() => GetRoute(ParseInput(inputLines), false);
 
-        protected override IConvertible PartTwo() => throw new NotImplementedException();
+        protected override IConvertible PartTwo() => GetRoute(ParseInput(inputLines), true);
+
+        private static int GetRoute(IEnumerable<(Turn, int)> directions, bool stopAtFirstRevisit)
+        {
+            var visited = new HashSet<AoCPoint>();
+            var currentDirection = Direction.Up;
+            var location = AoCPoint.Origin();
+            visited.Add(location);
+
+            foreach (var (turn, distance) in directions)
+            {
+                currentDirection = currentDirection.TurnTo(turn);
+
+                for (int i = 0; i < distance; i++)
+                {
+                    location = location.MoveTo(currentDirection);
+
+                    if (visited.Contains(location))
+                    {
+                        if (stopAtFirstRevisit) { return GetDistance(location); }
+                        else { continue; }
+                    }
+
+                    visited.Add(location);
+                }
+            }
+
+            return GetDistance(location);
+        }
+
+        private static int GetDistance(AoCPoint point) => Math.Abs(point.X) + Math.Abs(point.Y);
+
+        private static IEnumerable<(Turn, int)> ParseInput(IEnumerable<string> inputLines) {
+            var items = inputLines.First().Split(", ");
+            var directions = new List<(Turn, int)>();
+
+            foreach (var item in items)
+            {
+                var turn = item[0] == 'L' ? Turn.Left : Turn.Right;
+                var distance = int.Parse(item.Substring(1));
+                directions.Add((turn, distance));
+            }
+
+            return directions;
+        }
     }
 }
