f182306f7a63e9c0180cd670c6f3eac925e2a9fd
EwoutIO <ewout@schrotenboer.nl>
2020-02-21 21:15:56 +0100

    Day 2: Bathroom Security

--- AoC.Y2016.Tests/Day02Tests.cs
+++ AoC.Y2016.Tests/Day02Tests.cs
@@ -8,7 +8,37 @@ namespace AoC.Y2016.Tests.Days
     public class Day02Tests
     {
         [TestMethod]
-        public void PartOneTest()
+        [DataRow(new string[] { "ULL", "RRDDD", "LURDL", "UUUUD" }, "1985")]
+        public void PartOneTest(string[] input, string expected)
+        {
+            // Arrange
+            var target = new Day02(input);
+
+            // Act
+            var result = target.Debug(Part.One);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [DataRow(new string[] { "ULL", "RRDDD", "LURDL", "UUUUD" }, "5DB3")]
+        public void PartTwoTest(string[] input, string expected)
+        {
+            // Arrange
+            var target = new Day02(input);
+
+            // Act
+            var result = target.Debug(Part.Two);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void PartOne()
         {
             // Arrange
             var target = new Day02();
@@ -18,10 +48,11 @@ namespace AoC.Y2016.Tests.Days
 
             // Assert
             Assert.IsNotNull(result);
+            Assert.AreEqual("38961", result);
         }
 
         [TestMethod]
-        public void PartTwoTest()
+        public void PartTwo()
         {
             // Arrange
             var target = new Day02();
@@ -31,6 +62,7 @@ namespace AoC.Y2016.Tests.Days
 
             // Assert
             Assert.IsNotNull(result);
+            Assert.AreEqual("46C92", result);
         }
     }
 }
--- AoC.Y2016/Day02/Day02.cs
+++ AoC.Y2016/Day02/Day02.cs
@@ -1,6 +1,8 @@
 ﻿using AoC.Helpers.Days;
+using AoC.Helpers.Utils;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC.Y2016.Days
 {
@@ -14,8 +16,54 @@ namespace AoC.Y2016.Days
         {
         }
 
-        protected override IConvertible PartOne() => throw new NotImplementedException();
+        protected override IConvertible PartOne() => GetCode(inputLines, FirstNumpad());
 
-        protected override IConvertible PartTwo() => throw new NotImplementedException();
+        protected override IConvertible PartTwo() => GetCode(inputLines, SecondNumpad());
+
+        private static string GetCode(IEnumerable<string> instructions, Dictionary<AoCPoint, char> numpad)
+        {
+            var code = string.Empty;
+            var pos = numpad.Single(n => n.Value == '5').Key;
+
+            foreach (var instruction in instructions)
+            {
+                foreach (var step in instruction)
+                {
+                    var next = step == 'U' ? pos.Up()
+                             : step == 'L' ? pos.Left()
+                             : step == 'D' ? pos.Down()
+                             : pos.Right();
+
+                    if (numpad.ContainsKey(next))
+                    {
+                        pos = next;
+                    }
+                }
+
+                code += numpad[pos];
+            }
+
+            return code;
+        }
+
+        private static Dictionary<AoCPoint, char> FirstNumpad() =>
+            new Dictionary<AoCPoint, char>()
+            {
+                [P(-1, -1)] = '1', [P( 0, -1)] = '2', [P( 1, -1)] = '3',
+                [P(-1,  0)] = '4', [P( 0,  0)] = '5', [P( 1,  0)] = '6',
+                [P(-1,  1)] = '7', [P( 0,  1)] = '8', [P( 1,  1)] = '9',
+            };
+
+        private static Dictionary<AoCPoint, char> SecondNumpad() =>
+            new Dictionary<AoCPoint, char>
+            {
+                                                        [P(0, -2)] = '1',
+                                     [P(-1, -1)] = '2', [P(0, -1)] = '3',  [P(1, -1)] = '4',
+                  [P(-2,  0)] = '5', [P(-1,  0)] = '6', [P(0,  0)] = '7',  [P(1,  0)] = '8', [P(2, 0)] = '9',
+                                     [P(-1,  1)] = 'A', [P(0,  1)] = 'B',  [P(1,  1)] = 'C',
+                                                        [P(0,  2)] = 'D',
+            };
+
+        private static AoCPoint P(int x, int y) => new AoCPoint(x, y);
     }
 }
