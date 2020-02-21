831fa56fac1e9dfaee68b2ab7947a8533a2204a2
EwoutIO <ewout@schrotenboer.nl>
2020-02-21 07:57:43 +0100

    Day 23: Opening the Turing Lock

--- AoC.Y2015/Day23/Day23.cs
+++ AoC.Y2015/Day23/Day23.cs
@@ -1,6 +1,7 @@
 ﻿using AoC.Helpers.Days;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC.Y2015.Days
 {
@@ -14,8 +15,55 @@ namespace AoC.Y2015.Days
         {
         }
 
-        protected override IConvertible PartOne() => throw new NotImplementedException();
+		protected override IConvertible PartOne() => RunProgram(ParseInput(inputLines), 0);
 
-        protected override IConvertible PartTwo() => throw new NotImplementedException();
-    }
+        protected override IConvertible PartTwo() => RunProgram(ParseInput(inputLines), 1);
+
+		private static int RunProgram(List<(string name, int reg, int val)> instructions, int input)
+		{
+			var registers = new int[2] { input, 0 };
+			var ip = 0;
+
+			while (ip >= 0 && ip < instructions.Count())
+			{
+				var (name, reg, val) = instructions[ip];
+				switch (name)
+				{
+					case "hlf": registers[reg] /= 2; ip++; break;
+					case "tpl": registers[reg] *= 3; ip++; break;
+					case "inc": registers[reg]++; ip++; break;
+					case "jmp": ip += val; break;
+					case "jie": ip += registers[reg] % 2 == 0 ? val : 1; break;
+					case "jio": ip += registers[reg] == 1 ? val : 1; break;
+				}
+			}
+
+			return registers[1];
+		}
+
+		private static List<(string name, int reg, int val)> ParseInput(IEnumerable<string> inputLines)
+		{
+			var instructions = new List<(string, int, int)>();
+
+			foreach (var line in inputLines)
+			{
+				var items = line.Replace(",", "").Split(' ');
+				switch (items[0])
+				{
+					case "jmp":
+						instructions.Add((items[0], -1, int.Parse(items[1])));
+						break;
+					case "jie":
+					case "jio":
+						instructions.Add((items[0], items[1] == "a" ? 0 : 1, int.Parse(items[2])));
+						break;
+					default:
+						instructions.Add((items[0], items[1] == "a" ? 0 : 1, -1));
+						break;
+				}
+			}
+
+			return instructions;
+		}
+	}
 }
