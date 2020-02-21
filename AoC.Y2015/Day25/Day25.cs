c39e8fd5007cd70e081412063ac89259a4886820
EwoutIO <ewout@schrotenboer.nl>
2020-02-21 19:43:19 +0100

    Day 25: Let It Snow

--- AoC.Y2015/Day25/Day25.cs
+++ AoC.Y2015/Day25/Day25.cs
@@ -1,6 +1,7 @@
 ﻿using AoC.Helpers.Days;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC.Y2015.Days
 {
@@ -14,8 +15,44 @@ namespace AoC.Y2015.Days
         {
         }
 
-        protected override IConvertible PartOne() => throw new NotImplementedException();
+        protected override IConvertible PartOne()
+        {
+            var (row, column) = ParseInput(inputLines);
+            return CalculateCode(row, column);
+        }
 
-        protected override IConvertible PartTwo() => throw new NotImplementedException();
+        protected override IConvertible PartTwo() => "Merry christmas!";
+
+        private static long CalculateCode(int rowTarget, int columnTarget)
+        {
+            int col = 1, row = 1, count = 1;
+            var num = 20151125L;
+
+            while (true)
+            {
+                for (int innerIndex = 1; innerIndex <= count; innerIndex++)
+                {
+                    if (row == rowTarget && col == columnTarget)
+                    {
+                        return num;
+                    }
+
+                    num = ((num * 252533) % 33554393);
+                    col++; row--;
+                }
+
+                count++;
+                col = 1;
+                row = count;
+            }
+        }
+
+        private static (int row, int column) ParseInput(IEnumerable<string> inputLines)
+        {
+            var items = inputLines.Single().Split(' ');
+            var column = items[^1].Replace(".", string.Empty);
+            var row = items[^3].Replace(",", string.Empty);
+            return (int.Parse(row), int.Parse(column));
+        }
     }
 }
