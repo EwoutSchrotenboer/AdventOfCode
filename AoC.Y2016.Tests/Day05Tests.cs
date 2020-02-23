ced8e341c5324748081f01586aa399887eb9901c
EwoutIO <ewout@schrotenboer.nl>
2020-02-23 16:20:39 +0100

    Day 5: How About a Nice Game of Chess?

--- AoC.Y2016.Tests/Day05Tests.cs
+++ AoC.Y2016.Tests/Day05Tests.cs
@@ -18,6 +18,7 @@ namespace AoC.Y2016.Tests.Days
 
             // Assert
             Assert.IsNotNull(result);
+            Assert.AreEqual("c6697b55", result);
         }
 
         [TestMethod]
@@ -31,6 +32,7 @@ namespace AoC.Y2016.Tests.Days
 
             // Assert
             Assert.IsNotNull(result);
+            Assert.AreEqual("8c35d1ab", result);
         }
     }
 }
--- AoC.Y2016/Day05/Day05.cs
+++ AoC.Y2016/Day05/Day05.cs
@@ -1,6 +1,12 @@
 ﻿using AoC.Helpers.Days;
+using AoC.Helpers.Utils;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AoC.Y2016.Days
 {
@@ -14,8 +20,40 @@ namespace AoC.Y2016.Days
         {
         }
 
-        protected override IConvertible PartOne() => throw new NotImplementedException();
+        protected override IConvertible PartOne() => GetPassword(inputLines.Single(), true);
 
-        protected override IConvertible PartTwo() => throw new NotImplementedException();
+        protected override IConvertible PartTwo() => GetPassword(inputLines.Single(), false);
+
+        private static string GetPassword(string input, bool staticPosition)
+        {
+            var md5 = MD5.Create();
+            var num = 0;
+            var password = new char[8];
+            var charactersFound = 0;
+
+            while (charactersFound < 8)
+            {
+                var inputBytes = Encoding.ASCII.GetBytes(input + num);
+                var hash = md5.ComputeHash(inputBytes);
+
+                if ((hash[0] == 0) && (hash[1] == 0) && (hash[2] >> 4 & 0xF) == 0)
+                {
+                    if (staticPosition)
+                    {
+                        password[charactersFound] = hash[2].ToString("x2")[1];
+                        charactersFound++;
+                    }
+                    else if (hash[2] >= 0 && hash[2] < 8 && password[hash[2]] == '\0')
+                    {
+                        password[hash[2]] = hash[3].ToString("x2")[0];
+                        charactersFound++;
+                    }
+                }
+
+                num++;
+            }
+
+            return new string(password);
+        }
     }
 }
