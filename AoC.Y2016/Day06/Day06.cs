71f2ba5809682b622151cb9aacb13f897dc30512
EwoutIO <ewout@schrotenboer.nl>
2020-02-23 16:47:31 +0100

    Day 6: Signals and Noise

--- AoC.Y2016/Day06/Day06.cs
+++ AoC.Y2016/Day06/Day06.cs
@@ -1,6 +1,7 @@
 ﻿using AoC.Helpers.Days;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC.Y2016.Days
 {
@@ -14,8 +15,41 @@ namespace AoC.Y2016.Days
         {
         }
 
-        protected override IConvertible PartOne() => throw new NotImplementedException();
+        protected override IConvertible PartOne() => GetMessage(inputLines.ToList(), true);
 
-        protected override IConvertible PartTwo() => throw new NotImplementedException();
+        protected override IConvertible PartTwo() => GetMessage(inputLines.ToList(), false);
+
+        private static string GetMessage(List<string> messages, bool getMostCommon)
+        {
+            var columns = new List<char[]>();
+            var message = string.Empty;
+
+            for (int i = 0; i < messages[0].Length; i++)
+            {
+                columns.Add(messages.Select(m => m[i]).ToArray());
+            }
+
+            foreach (var column in columns)
+            {
+                var characters = new Dictionary<char, int>();
+
+                foreach (var character in column)
+                {
+                    if (!characters.ContainsKey(character)) { characters.Add(character, 0); }
+                    characters[character]++;
+                }
+
+                if (getMostCommon)
+                {
+                    message += characters.OrderByDescending(c => c.Value).First().Key;
+                }
+                else
+                {
+                    message += characters.OrderBy(c => c.Value).First().Key;
+                }
+            }
+
+            return message;
+        }
     }
 }
