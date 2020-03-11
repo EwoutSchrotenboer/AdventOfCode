660fd5fc86fb233efec992e58d635a6e5ea48f5f
EwoutIO <ewout@schrotenboer.nl>
2020-03-11 08:01:50 +0100

    Day 18: Like a Rogue

--- AoC.Y2016.Tests/Day18Tests.cs
+++ AoC.Y2016.Tests/Day18Tests.cs
@@ -18,6 +18,7 @@ namespace AoC.Y2016.Tests.Days
 
             // Assert
             Assert.IsNotNull(result);
+            Assert.AreEqual(2035, result);
         }
 
         [TestMethod]
@@ -31,6 +32,7 @@ namespace AoC.Y2016.Tests.Days
 
             // Assert
             Assert.IsNotNull(result);
+            Assert.AreEqual(20000577, result);
         }
     }
 }
--- AoC.Y2016/Day18/Day18.cs
+++ AoC.Y2016/Day18/Day18.cs
@@ -1,6 +1,7 @@
 ﻿using AoC.Helpers.Days;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC.Y2016.Days
 {
@@ -14,8 +15,49 @@ namespace AoC.Y2016.Days
         {
         }
 
-        protected override IConvertible PartOne() => throw new NotImplementedException();
+        protected override IConvertible PartOne() => GenerateMap(ParseInput(inputLines), 40).Sum(l => l.Count(c => c == false));
 
-        protected override IConvertible PartTwo() => throw new NotImplementedException();
+        protected override IConvertible PartTwo() => GenerateMap(ParseInput(inputLines), 400000).Sum(l => l.Count(c => c == false));
+
+        private List<bool[]> GenerateMap(bool[] initial, int rows)
+        {
+            var map = new List<bool[]>() { initial };
+
+            var current = initial;
+
+            for (int i = 0; i < rows - 1; i++)
+            {
+                var next = NextRow(current);
+                map.Add(next);
+                current = next;
+            }
+
+            return map;
+        }
+
+        private static bool[] NextRow(bool[] current)
+        {
+            var next = new bool[current.Length];
+
+            next[0] = IsTrap(false, current[0], current[1]);
+
+            for (int i = 1; i < current.Length - 1; i++)
+            {
+                next[i] = IsTrap(current[i - 1], current[i], current[i + 1]);
+            }
+
+            next[^1] = IsTrap(current[^2], current[^1], false);
+
+            return next;
+        }
+
+        private static bool IsTrap(bool left, bool center, bool right) =>
+            (left && center && !right)
+            || (!left && center && right)
+            || (left && !center && !right)
+            || (!left && !center && right);
+
+
+        private static bool[] ParseInput(IEnumerable<string> inputLines) => inputLines.Single().Select(c => c == '^').ToArray();
     }
 }
