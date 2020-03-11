e2c9669adea47655d1dc149dd138b7228d9f6dd2
EwoutIO <ewout@schrotenboer.nl>
2020-03-11 08:27:54 +0100

    Day 22: Grid Computing

--- AoC.Y2016/Day22/Day22.cs
+++ AoC.Y2016/Day22/Day22.cs
@@ -1,6 +1,7 @@
 ﻿using AoC.Helpers.Days;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC.Y2016.Days
 {
@@ -14,8 +15,57 @@ namespace AoC.Y2016.Days
         {
         }
 
-        protected override IConvertible PartOne() => throw new NotImplementedException();
+        protected override IConvertible PartOne() => CountViablePairs(ParseInput(inputLines));
 
-        protected override IConvertible PartTwo() => throw new NotImplementedException();
-    }
+		protected override IConvertible PartTwo() => ShortestDataRoute(ParseInput(inputLines));
+
+		private static int ShortestDataRoute(List<StorageNode> nodes)
+		{
+			var src = nodes.Single(n => n.X == nodes.Max(n => n.X) && n.Y == 0);
+			var dst = nodes.Single(n => n.X == 0 && n.Y == 0);
+			var empty = nodes.Single(n => n.Used == 0);
+
+			// Note: no support for blocking big drives at this time.
+			var steps = 0;
+
+			// move empty disk to the left of the source disk
+			if (empty.X < src.X) { steps += src.X - 1 - empty.X; }
+			else { steps += empty.X - src.X + 1; }
+
+			steps += empty.Y - 1;
+
+			// calculate how many steps it is to the destination (0 in this case)
+			var repeats = src.X - dst.X;
+			// 1.    | 2.    | 3.    | 4.    | 5.    |
+			// . S _ | . S . | . S . | . S . | _ S . |
+			// . . . | . . _ | . _ . | _ . . | . . . |
+
+			steps += repeats * 5;
+
+			// Last step, we do not need the five steps
+			steps += 1;
+
+			return steps;
+		}
+
+		private static int CountViablePairs(List<StorageNode> nodes)
+		{
+			var count = 0;
+
+			for (int a = 0; a < nodes.Count(); a++)
+			{
+				if (nodes[a].Used == 0) { continue; }
+
+				for (int b = 0; b < nodes.Count(); b++)
+				{
+					if (nodes[a].X == nodes[b].X && nodes[a].Y == nodes[b].Y) { continue; }
+					if (nodes[a].Used <= nodes[b].Available) { count++; }
+				}
+			}
+
+			return count;
+		}
+
+		private static List<StorageNode> ParseInput(IEnumerable<string> inputLines) => inputLines.Skip(2).Select(l => new StorageNode(l)).ToList();
+	}
 }
---
+++ AoC.Y2016/Day22/StorageNode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AoC.Y2016.Days
+{
+    internal class StorageNode
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Size { get; set; }
+        public int Used { get; set; }
+        public int Available { get; set; }
+
+        public StorageNode(string input)
+        {
+            var items = input
+                .Replace("x", "")
+                .Replace("y", "")
+                .Replace("T", "")
+                .Replace("%", "")
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            var coords = items[0].Split('-');
+            X = int.Parse(coords[1]);
+            Y = int.Parse(coords[2]);
+
+            Size = int.Parse(items[1]);
+            Used = int.Parse(items[2]);
+            Available = int.Parse(items[3]);
+        }
+    }
+}
