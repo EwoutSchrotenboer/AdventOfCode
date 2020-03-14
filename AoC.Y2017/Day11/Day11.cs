a665a62017d6939d23fce8c6c2963ceb949ae57c
EwoutIO <ewout@schrotenboer.nl>
2020-03-14 17:17:06 +0100

    Remove copy/paste variable

--- AoC.Y2017/Day11/Day11.cs
+++ AoC.Y2017/Day11/Day11.cs
@@ -7,8 +7,6 @@ namespace AoC.Y2017.Days
 {
     public class Day11 : BaseDay
     {
-        private readonly int serialInput = 3463;
-
         public Day11() : base(2017, 11)
         {
         }
