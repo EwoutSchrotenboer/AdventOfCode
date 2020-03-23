e93218cb9b293ede1b57e81cddbcebc59fd8d282
EwoutIO <ewout@schrotenboer.nl>
2020-03-23 18:26:43 +0100

    Day 24: Electromagnetic Moat

--- AoC.Y2017.Tests/Day24Tests.cs
+++ AoC.Y2017.Tests/Day24Tests.cs
@@ -8,7 +8,37 @@ namespace AoC.Y2017.Tests.Days
     public class Day24Tests
     {
         [TestMethod]
-        public void PartOneTest()
+        [DataRow(new string[] { "0/2", "2/2", "2/3", "3/4", "3/5", "0/1", "10/1", "9/10" }, 31)]
+        public void PartOneTest(string[] input, int expected)
+        {
+            // Arrange
+            var target = new Day24(input);
+
+            // Act
+            var result = target.Debug(Part.One);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [DataRow(new string[] { "0/2", "2/2", "2/3", "3/4", "3/5", "0/1", "10/1", "9/10" }, 19)]
+        public void PartTwoTest(string[] input, int expected)
+        {
+            // Arrange
+            var target = new Day24(input);
+
+            // Act
+            var result = target.Debug(Part.Two);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void PartOne()
         {
             // Arrange
             var target = new Day24();
@@ -18,10 +48,11 @@ namespace AoC.Y2017.Tests.Days
 
             // Assert
             Assert.IsNotNull(result);
+            Assert.AreEqual(1868, result);
         }
 
         [TestMethod]
-        public void PartTwoTest()
+        public void PartTwo()
         {
             // Arrange
             var target = new Day24();
@@ -31,6 +62,7 @@ namespace AoC.Y2017.Tests.Days
 
             // Assert
             Assert.IsNotNull(result);
+            Assert.AreEqual(1841, result);
         }
     }
 }
--- AoC.Y2017/Day24/Day24.cs
+++ AoC.Y2017/Day24/Day24.cs
@@ -1,6 +1,7 @@
 ﻿using AoC.Helpers.Days;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC.Y2017.Days
 {
@@ -14,8 +15,91 @@ namespace AoC.Y2017.Days
         {
         }
 
-        protected override IConvertible PartOne() => throw new NotImplementedException();
+        protected override IConvertible PartOne()
+        {
+            var components = ParseInput(inputLines);
+            var bridges = BuildBridges(components);
+            return GetStrongestBridge(bridges);
+        }
 
-        protected override IConvertible PartTwo() => throw new NotImplementedException();
+        protected override IConvertible PartTwo()
+        {
+            var components = ParseInput(inputLines);
+            var bridges = BuildBridges(components);
+            var longest = GetLongestBridges(bridges, bridges.Max(b => b.Count()));
+            return GetStrongestBridge(longest);
+        }
+
+        private static List<List<(int, int)>> BuildBridges(List<(int A, int B)> components)
+        {
+            var startItems = components.Where(c => c.A == 0 || c.B == 0);
+            var bridges = new List<List<(int A, int B)>>();
+
+            foreach (var start in startItems)
+            {
+                var currentComponents = new List<(int A, int B)>() { start };
+                var current = start.A == 0 ? start.B : start.A;
+                var usableComponents = components.Where(c => c != start).ToList();
+                bridges.AddRange(CreateBridges(currentComponents, current, usableComponents));
+            }
+
+            return bridges;
+        }
+
+        private static List<List<(int, int)>> CreateBridges(List<(int, int)> currentComponents, int current, List<(int A, int B)> usableComponents)
+        {
+            var bridges = new List<List<(int, int)>>();
+            bridges.Add(currentComponents);
+
+            var viableComponents = usableComponents.Where(c => c.A == current || c.B == current);
+
+            if (!viableComponents.Any())
+            {
+                return bridges;
+            }
+
+            foreach (var viableComponent in viableComponents)
+            {
+                var newBridge = currentComponents.Select(c => c).ToList();
+                newBridge.Add(viableComponent);
+                var nextPort = viableComponent.A == current ? viableComponent.B : viableComponent.A;
+                var nextComponents = usableComponents.Where(c => c != viableComponent).ToList();
+                bridges.AddRange(CreateBridges(newBridge, nextPort, nextComponents));
+            }
+
+            return bridges;
+        }
+
+        private static List<List<(int, int)>> GetLongestBridges(List<List<(int A, int B)>> bridges, int maxLength) => bridges.Where(b => b.Count() == maxLength).ToList();
+
+        private static int GetStrongestBridge(List<List<(int A, int B)>> bridges)
+        {
+            var strongest = 0;
+
+            foreach (var bridge in bridges)
+            {
+                var strength = bridge.Sum(b => b.A + b.B);
+
+                if (strength > strongest)
+                {
+                    strongest = strength;
+                }
+            }
+
+            return strongest;
+        }
+
+        private static List<(int, int)> ParseInput(IEnumerable<string> inputLines)
+        {
+            var components = new List<(int, int)>();
+
+            foreach (var line in inputLines)
+            {
+                var items = line.Split('/');
+                components.Add((int.Parse(items[0]), int.Parse(items[1])));
+            }
+
+            return components;
+        }
     }
 }
